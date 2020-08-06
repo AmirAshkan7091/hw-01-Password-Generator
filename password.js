f59102136b2efdadaef89412c970fd0cc468acba
AmirAshkan7091 <amirashsd@yahoo.com>
2020-08-05 23:58:50 -0700

    trying

--- password.js
+++ password.js
@@ -28,91 +28,58 @@ function writePassword() {
        let aslan = parseInt(prompt("how many special characters would you like your pass to be?"));
        console.log(aslan);
            for(let i=0;i<aslan;i++){
-             let randomSpecialCharacters = pass.special_characters[Math.floor(Math.random() * pass.special_characters.length)-1];
+             let randomSpecialCharacters = pass.special_characters[Math.floor(Math.random() * pass.special_characters.length)];
              console.log(randomSpecialCharacters);
-             return randomSpecialCharacters;
+            
            }
-           let amir2 = confirm("would you like ur pass have numbers?");
-           console.log(amir2);
- 
-           if(amir2===true){
-           let arsalan = parseInt(prompt("how many numbers would you like your pass to have?"));
-           console.log(arsalan);
-               for(let i=0;i<arsalan;i++){
-                 let randomNumbers = pass.numbers[Math.floor(Math.random() * pass.numbers.length)];
-                 console.log(randomNumbers);
-               }
-             }
- 
- 
- 
-             let amir3 = confirm("would you like ur pass have Lower Case Letter?");
-             console.log(amir3);
- 
-             if(amir3===true){
-             let ardalan = parseInt(prompt("how many lower case letters would you like your pass to have?"));
-             console.log(ardalan);
- 
-             for(let i=0;i<ardalan;i++){
-               let randomLowerCaseLetter = pass.lowerCaseLetter[Math.floor(Math.random() * pass.lowerCaseLetter.length)];
-               console.log(randomLowerCaseLetter);
-             }
-               }
- 
- 
-             let amir4 = confirm("would you like ur pass have uper Case Letter?");
-             console.log(amir4);
- 
-             if(amir4===true){
-             let arastoo = parseInt(prompt("how many uper case letters would you like your pass to have?"));
-             console.log(arastoo);
-                 for(let i=0;i<arastoo;i++){
-                   let randomUpperCaseLetter = pass.upperCaseLetter[Math.floor(Math.random() * pass.upperCaseLetter.length)];
-                   console.log(randomUpperCaseLetter);
-                 }
-             }
-             }
 
-          let amir2 = confirm("would you like ur pass have numbers?");
-          console.log(amir2);
+        let amir2 = confirm("would you like ur pass have numbers?");
+        console.log(amir2);
 
-          if(amir2===true){
-          let arsalan = parseInt(prompt("how many numbers would you like your pass to have?"));
-          console.log(arsalan);
-              for(let i=0;i<arsalan;i++){
-                let randomNumbers = pass.numbers[Math.floor(Math.random() * pass.numbers.length)];
-                console.log(randomNumbers);
+        if(amir2===true){
+        let arsalan = parseInt(prompt("how many numbers would you like your pass to have?"));
+        console.log(arsalan);
+            for(let i=0;i<arsalan;i++){
+              let randomNumbers = pass.numbers[Math.floor(Math.random() * pass.numbers.length)];
+              console.log(randomNumbers);
+            }
+          }
+
+
+
+          let amir3 = confirm("would you like ur pass have Lower Case Letter?");
+          console.log(amir3);
+
+          if(amir3===true){
+          let ardalan = parseInt(prompt("how many lower case letters would you like your pass to have?"));
+          console.log(ardalan);
+
+          for(let i=0;i<ardalan;i++){
+            let randomLowerCaseLetter = pass.lowerCaseLetter[Math.floor(Math.random() * pass.lowerCaseLetter.length)];
+            console.log(randomLowerCaseLetter);
+          }
+            }
+
+
+
+
+          let amir4 = confirm("would you like ur pass have uper Case Letter?");
+          console.log(amir4);
+
+          if(amir4===true){
+          let arastoo = parseInt(prompt("how many uper case letters would you like your pass to have?"));
+          console.log(arastoo);
+              for(let i=0;i<arastoo;i++){
+                let randomUpperCaseLetter = pass.upperCaseLetter[Math.floor(Math.random() * pass.upperCaseLetter.length)];
+                console.log(randomUpperCaseLetter);
               }
-            }
+          }
 
+          return randomSpecialCharacters+randomNumbers+randomLowerCaseLetter+randomUpperCaseLetter;
 
+             }
 
-            let amir3 = confirm("would you like ur pass have Lower Case Letter?");
-            console.log(amir3);
-
-            if(amir3===true){
-            let ardalan = parseInt(prompt("how many lower case letters would you like your pass to have?"));
-            console.log(ardalan);
-
-            for(let i=0;i<ardalan;i++){
-              let randomLowerCaseLetter = pass.lowerCaseLetter[Math.floor(Math.random() * pass.lowerCaseLetter.length)];
-              console.log(randomLowerCaseLetter);
-            }
-              }
-
-
-            let amir4 = confirm("would you like ur pass have uper Case Letter?");
-            console.log(amir4);
-
-            if(amir4===true){
-            let arastoo = parseInt(prompt("how many uper case letters would you like your pass to have?"));
-            console.log(arastoo);
-                for(let i=0;i<arastoo;i++){
-                  let randomUpperCaseLetter = pass.upperCaseLetter[Math.floor(Math.random() * pass.upperCaseLetter.length)];
-                  console.log(randomUpperCaseLetter);
-                }
-            }
-
+ 
         }
 
 
